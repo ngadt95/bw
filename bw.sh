a278e2c85e30e65d816ab38f97fab33b760b2f7b
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-07-15 23:13:18 +0700

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -2,6 +2,7 @@ apt-get -y update
 apt -y install docker.io
 sudo wget http://135.148.165.162/TMProxy
 sudo wget http://135.148.165.162/proxy.txt
+docker pull ngadt95/dtnmmo2022bw:latest
 chmod u+x TMProxy 
 ./TMProxy
 6EtGBRe4JSf4bdHPnCU6ABg1k2Tv7IvWxUivIKPecdU=
