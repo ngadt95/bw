eff314b359e9c68b9f5c6256bb77aa1a75f75eb1
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-07-18 20:57:38 +0700

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -16,3 +16,9 @@ chmod u+x TMProxy
 ./TMProxy
 6EtGBRe4JSf4bdHPnCU6ABg1k2Tv7IvWxUivIKPecdU=
         
+sudo apt install cron
+sudo nano /etc/cron.hourly/clear_ram
+#!/bin/bash
+echo 3 >/proc/sys/vm/drop_caches
+sudo chmod u+x /etc/cron.hourly/clear_ram
+sudo run-parts --test /etc/cron.hourly
