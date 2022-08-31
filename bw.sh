0ccedab474103d7c50e194195112b1742ffe52d8
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-08-31 18:43:08 +0700

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -1,7 +1,7 @@
 apt-get -y update
 apt -y install docker.io
 docker pull ngadt95/dtnmmo2022traff
-sudo wget http://135.148.165.162/TMProxy
+sudo wget http://51.79.186.224/TMProxy
 sudo apt install cron
 sudo nano /etc/cron.hourly/clear_ram
 #!/bin/bash
@@ -14,16 +14,6 @@ sudo crontab -e
 1 */10 * * * /sbin/shutdown -r now
 1 */12 * * * /sbin/shutdown -r now
 
-sudo wget http://135.148.165.162/proxy1.txt
-sudo wget http://135.148.165.162/proxy2.txt
-sudo wget http://135.148.165.162/proxy3.txt
-sudo wget http://135.148.165.162/proxy4.txt
-sudo wget http://135.148.165.162/proxy5.txt
-sudo wget http://135.148.165.162/proxy6.txt
-sudo wget http://135.148.165.162/proxy7.txt
-sudo wget http://135.148.165.162/proxy8.txt
-sudo wget http://135.148.165.162/proxy9.txt
-sudo wget http://135.148.165.162/proxy10.txt
 chmod u+x TMProxy 
 ./TMProxy
 6EtGBRe4JSf4bdHPnCU6ABg1k2Tv7IvWxUivIKPecdU=
