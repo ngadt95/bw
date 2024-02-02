904de5bd42e49d1894256b38fffbcce36816b4e6
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2024-02-02 10:17:48 +0700

    Update bw.sh

--- bw.sh
+++ bw.sh
@@ -5,7 +5,8 @@ sudo wget http://dtn.vnz-team.com/ubuntu-14.04-desktop-i386.iso
 http://s3.vnz-team.com/dtn2401.raw
 sudo wget http://104.161.23.254/TMProxy
 sudo wget http://104.161.23.254/block.txt
-sudo wget http://104.161.23.254/txt/11.txt
+sudo wget http://104.161.23.254/txt.zip
+unzip txt.zip
 sudo apt install cron
 sudo nano /etc/cron.hourly/clear_ram
 #!/bin/bash
